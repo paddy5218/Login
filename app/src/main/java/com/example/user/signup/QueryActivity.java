f59f655921d8101bd24d3cead1dd1c39d2ac7e93
user <paddy10401@gmail.com>
2017-07-18 17:20:25 +0800

    登入跟傳送訊息

---
+++ app/src/main/java/com/example/user/signup/QueryActivity.java
@@ -0,0 +1,22 @@
+package com.example.user.signup;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.Toast;
+
+public class QueryActivity extends AppCompatActivity {
+
+    private ListView listView;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_query);
+        listView = (ListView) findViewById(R.id.listview);
+
+    }
+}
